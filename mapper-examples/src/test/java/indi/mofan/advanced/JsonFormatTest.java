ac6b52656996ad6385cc5b345c14a840e3e4a519
mofan <cy.mofan@foxmail.com>
2024-02-04 18:29:16 +0800

    Case-Insensitive Deserialization

--- mapper-examples/src/test/java/indi/mofan/advanced/JsonFormatTest.java
+++ mapper-examples/src/test/java/indi/mofan/advanced/JsonFormatTest.java
@@ -1,14 +1,17 @@
 package indi.mofan.advanced;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.SneakyThrows;
 import net.javacrumbs.jsonunit.assertj.JsonAssertions;
+import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.Test;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -16,7 +19,7 @@ import java.util.Date;
  * @date 2024/2/3 16:54
  * @link <a href="https://www.baeldung.com/jackson-jsonformat">Guide to @JsonFormat in Jackson</a>
  */
-public class JsonFormatTest {
+public class JsonFormatTest implements WithAssertions {
 
     private static final JsonMapper MAPPER = JsonMapper.builder().build();
 
@@ -30,10 +33,6 @@ public class JsonFormatTest {
          * 2024-02-03
          */
         private Date createdDate = new Date(1706803200000L);
-
-        public Date getCreatedDate() {
-            return this.createdDate;
-        }
 
         static User createUser() {
             User user = new User();
@@ -65,8 +64,8 @@ public class JsonFormatTest {
     }
 
     private static abstract class UserMixIn2 {
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-        public abstract Date getCurrentDate();
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Shanghai")
+        public abstract Date getCreatedDate();
     }
 
     @Test
@@ -87,5 +86,51 @@ public class JsonFormatTest {
                 }
                 """;
         JsonAssertions.assertThatJson(result).isEqualTo(expectJson);
+
+        mapper = JsonMapper.builder()
+                .addMixIn(User.class, UserMixIn2.class)
+                .build();
+        result = mapper.writeValueAsString(user);
+        // language=JSON
+        expectJson = """
+                {
+                  "firstName": "mo",
+                  "lastName": "fan",
+                  "createdDate": "2024-02-02"
+                }
+                """;
+        JsonAssertions.assertThatJson(result).isEqualTo(expectJson);
+    }
+
+    // language=json
+    static final String JSON_STRING = """
+            {
+              "FIRSTNAME": "mo",
+              "lastname": "fan",
+              "cReAtEdDaTe": "2024-02-02@00:00:00.000+0800"
+            }""";
+
+    @JsonFormat(with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+    static class UserIgnoreCaseMixIn {
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss.SSSZ", timezone = "Asia/Shanghai")
+        private Date createdDate;
+    }
+
+    @Test
+    @SneakyThrows
+    public void testCaseInsensitiveDeserialization() {
+        assertThatThrownBy(() -> JsonMapper.builder().build().readValue(JSON_STRING, User.class))
+                .isInstanceOf(UnrecognizedPropertyException.class);
+
+        JsonMapper mapper = JsonMapper.builder()
+                .addMixIn(User.class, UserIgnoreCaseMixIn.class)
+                .build();
+        User user = mapper.readValue(JSON_STRING, User.class);
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd@HH:mm:ss.SSSZ");
+        Date date = fmt.parse("2024-02-02@00:00:00.000+0800");
+        assertThat(user).isNotNull()
+                .returns("mo", User::getFirstName)
+                .returns("fan", User::getLastName)
+                .returns(date, User::getCreatedDate);
     }
 }
